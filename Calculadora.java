0f0f51ac1be00e614cdd20af541a9d8acac1ac0e
Guilherme <67837773+gdsouza08@users.noreply.github.com>
2021-06-06 21:52:05 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) throws IOException {
+   
+        System.out.println("Escolhha a operação a ser realizada: ");
+        System.out.println("[1] para adição");
+        System.out.println("[2] para subtração");
+        System.out.println("[3] para multiplicação");
+        System.out.println("[4] pra divisão");
+        System.out.println("[5] para sair ");
+        System.out.print("Qual a sua opção? ");
+       
+        Scanner leitor = new Scanner(System.in);
+        int opcao = leitor.nextInt();
+
+        if (opcao == 5) {
+            System.out.println("Saindo do programa...");
+            System.exit(0);
+        }
+
+        System.out.print("Digite um valor: ");
+        int n1 = leitor.nextInt();
+        System.out.print("Digite outro valor: ");
+        int n2 = leitor.nextInt();
+       
+        switch (opcao) {
+            case 1:
+            int adicao = n1 + n2;
+            System.out.println(n1 + " + " + n2 + " = " + adicao);
+            break;
+            case 2:
+            int subtracao = n1 - n2;
+            System.out.println(n1 + " - " + n2 + " = " + subtracao);
+            break;
+            case 3:
+            int multiplicação = n1 * n2;
+            System.out.println(n1 + " * " + n2 + " = " + multiplicação);
+            break;
+            case 4:
+            int  divisão = n1 / n2;
+            System.out.println(n1 + " / " + n2 + " = " + divisão);
+            break;
+            default:
+            System.out.println("Opçao inválida.");
+        }
+    }
+}
